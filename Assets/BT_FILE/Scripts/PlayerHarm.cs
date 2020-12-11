16c399c9f884e9344978b6156b918a35f63960da
BadToast <ship2042@gmail.com>
2020-12-11 21:35:22 +0900

    2

---
+++ Assets/BT_FILE/Scripts/PlayerHarm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class PlayerHarm : MonoBehaviour
+{
+    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        
+    }
+}
