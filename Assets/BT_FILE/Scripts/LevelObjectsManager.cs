70048137d1529455c2b788ee3f2199c4e3d3f31c
BadToast <ship2042@gmail.com>
2020-12-11 22:25:56 +0900

    4 - First Merged

--- Assets/BT_FILE/Scripts/LevelObjectsManager.cs
+++ Assets/BT_FILE/Scripts/LevelObjectsManager.cs
@@ -1,30 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class LevelObjectsManager : MonoBehaviour
 {
-    private static LevelObjectsManager instance = null;
+    private static LevelObjectsManager _instance = null;
     
     public static LevelObjectsManager Instance
     {
         get
         { 
-            return instance; 
+            return _instance; 
         }
     }
      
-    public delegate void OnPlayerHurt();
+    public delegate void OnPlayerHurt();  
+
+    public OnPlayerHurt ONPlayerHurt; // 여기에 플레이어가 피해를 입었을때 호출되는 함수를 추가해주세요
+    
+    [InfoBox( "반드시 플레이어 레이어를 넣어주세요",InfoMessageType.Warning)]
+    public LayerMask playerLayer;
     
     private void Awake()
     {
-        if (instance != null && instance != this) 
+        if (_instance != null && _instance != this) 
         {
             Destroy(this.gameObject);
         }
  
-        instance = this;
+        _instance = this;
     }
-
+    
     
 }
--- Assets/BT_FILE/Scripts/PlayerHarm.cs
+++ Assets/BT_FILE/Scripts/PlayerHarm.cs
@@ -6,9 +6,18 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class PlayerHarm : MonoBehaviour
 {
-    
+    private LevelObjectsManager loManager;
+
+    private void Awake()
+    {
+        loManager = LevelObjectsManager.Instance;
+    }
+
     private void OnCollisionEnter2D(Collision2D other)
     {
-        
+        if (other.gameObject.layer == loManager.playerLayer.value) // 버그 가능성 있음
+        {
+            loManager.ONPlayerHurt();
+        }
     }
 }
